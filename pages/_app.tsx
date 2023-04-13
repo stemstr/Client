84e5f21a9b5d487cf6dec7349d3be2dd64531378
bndw <ben@bdw.to>
2023-04-12 20:11:08 -0700

    fix: disable zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,7 +51,7 @@ function App(props: AppProps & { colorScheme: ColorScheme }) {
         <title>Stemstr</title>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
+          content="minimum-scale=1, initial-scale=1, maximum-scale=1.0, user-scalable=no, width=device-width"
         />
         <meta
           name="description"
