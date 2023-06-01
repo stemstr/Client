a926278057c72ef4f7c7240795dcc28ec75efc0b
Sam Samskies <samsamskies@gmail.com>
2023-05-31 19:28:19 -0500

    fix profile page width

--- components/ApplicationContainer/ApplicationContainer.tsx
+++ components/ApplicationContainer/ApplicationContainer.tsx
@@ -1,4 +1,5 @@
 import { AppShell } from "@mantine/core";
+import { useRouter } from "next/router";
 
 import BottomNavigation from "../BottomNavigation/BottomNavigation";
 import PostSheet from "../PostSheet/PostSheet";
@@ -6,25 +7,31 @@ import FileDropOverlay from "../FileDropOverlay/FileDropOverlay";
 
 export const ApplicationContainer = ({
   children,
-}: React.PropsWithChildren<{}>) => (
-  <AppShell
-    padding={0}
-    styles={{
-      root: {
-        width: "100%",
-        height: "100vh",
-        margin: "auto",
-      },
-      main: {
-        width: "100%",
-        height: "100vh",
-      },
-    }}
-    fixed
-    footer={<BottomNavigation />}
-  >
-    {children}
-    <PostSheet />
-    <FileDropOverlay />
-  </AppShell>
-);
+}: React.PropsWithChildren<{}>) => {
+  const router = useRouter();
+  console.log(router.pathname);
+
+  return (
+    <AppShell
+      padding={0}
+      styles={{
+        root: {
+          width: "100%",
+          maxWidth: router.pathname === "/" ? "100%" : 600,
+          height: "100vh",
+          margin: "auto",
+        },
+        main: {
+          width: "100%",
+          height: "100vh",
+        },
+      }}
+      fixed
+      footer={<BottomNavigation />}
+    >
+      {children}
+      <PostSheet />
+      <FileDropOverlay />
+    </AppShell>
+  );
+};
