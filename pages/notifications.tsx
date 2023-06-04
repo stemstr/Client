74a943c41a247028d84f88080d64b60d7e9a9fd3
Michael Hall <michaelhall@Michaels-MacBook-Pro.local>
2023-06-04 16:35:05 -0400

    add header to notifications page

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,14 +1,18 @@
-import { Stack } from "@mantine/core";
+import { Box, Stack } from "@mantine/core";
 import { useNotifications } from "ndk/hooks/useNotifications";
 import { useSelector } from "react-redux";
 import { AppState } from "store/Store";
 import NotificationView from "components/NotificationView/NotificationView";
+import HomeFeedHeader from "components/HomeFeedHeader/HomeFeedHeader";
 
 export default function Notifications() {
   const authState = useSelector((state: AppState) => state.auth);
   const notifications = useNotifications({ pubkey: authState.pk as string });
   return (
-    <Stack>
+    <Stack spacing={0}>
+      <Box m="auto" pl="md" pr="md" w="100%" sx={{ maxWidth: 600 }}>
+        <HomeFeedHeader />
+      </Box>
       {Array.from(notifications.values())
         .sort((a, b) => b.created_at - a.created_at)
         .map((notification) => (
