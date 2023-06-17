a044adddbc7022f239be87e9fccac15e51ccaec4
Michael Hall <michaelhall@Michaels-MacBook-Pro.local>
2023-06-17 12:49:57 -0400

    add lnurl field

--- components/EditProfileForm/LNURLFieldGroup.tsx
+++ components/EditProfileForm/LNURLFieldGroup.tsx
@@ -2,9 +2,33 @@ import FieldGroup from "components/FieldGroups/FieldGroup";
 import useStyles from "./EditProfileForm.styles";
 import { WalletIcon } from "icons/StemstrIcon";
 import { TextInput } from "@mantine/core";
+import { ChangeEvent, useMemo } from "react";
+import { UseFormReturnType } from "@mantine/form";
+import { NDKUserProfile } from "@nostr-dev-kit/ndk";
+import { isValidLUD16 } from "utils/lightning";
 
-export default function LNURLField(props: any) {
+type LNURLFieldProps = {
+  form: UseFormReturnType<
+    NDKUserProfile,
+    (values: NDKUserProfile) => NDKUserProfile
+  >;
+};
+
+export default function LNURLField({ form }: LNURLFieldProps) {
   const { classes } = useStyles();
+  const lud06Props = useMemo(() => form.getInputProps("lud06"), [form]);
+  const lud16Props = useMemo(() => form.getInputProps("lud16"), [form]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let val = event.target.value;
+    if (isValidLUD16(val)) {
+      lud06Props.onChange("");
+      lud16Props.onChange(val);
+    } else {
+      lud06Props.onChange(val);
+      lud16Props.onChange("");
+    }
+  };
 
   return (
     <FieldGroup
@@ -18,7 +42,8 @@ export default function LNURLField(props: any) {
         classNames={{
           input: classes.textInput,
         }}
-        {...props}
+        {...(lud06Props.value ? lud06Props : lud16Props)}
+        onChange={handleChange}
       />
     </FieldGroup>
   );
