d9da400f4888b6d78db5964742b525cef7269c43
Sam Samskies <samsamskies@gmail.com>
2023-07-03 14:00:16 -0500

    cache user relay list

--- ndk/inMemoryCacheAdapter.ts
+++ ndk/inMemoryCacheAdapter.ts
@@ -10,6 +10,8 @@ const eventsCache: Record<string, Record<string, NostrEvent>> = {};
 
 const profileEventsCache: Record<string, NostrEvent> = {};
 
+const relayListCache: Record<string, NostrEvent> = {};
+
 export const getCachedProfile = (pubkey: string, ndk?: NDK) => {
   const cachedProfile = profileEventsCache[pubkey];
 
@@ -45,44 +47,51 @@ const inMemoryCacheAdapter = {
     const { filter } = subscription;
     const { authors, kinds } = filter;
 
-    // currently only supporting profile caching and authors and kinds are available for profile caching
+    // currently only supporting profile and relay list caching.
+    // authors and kinds are available for profile caching.
     if (!authors || !kinds) {
       return;
     }
 
     authors.forEach((pubkey: string) => {
-      if (!kinds.includes(Kind.Metadata)) {
-        return;
-      }
+      kinds.forEach((kind: number) => {
+        if (![Kind.Metadata, Kind.RelayList].includes(kind)) {
+          return;
+        }
 
-      const cachedProfile = profileEventsCache[pubkey];
+        const cache =
+          kind === Kind.Metadata ? profileEventsCache : relayListCache;
+        const cachedEvent = cache[pubkey];
 
-      if (cachedProfile) {
-        const ndkEvent = new NDKEvent(subscription.ndk, cachedProfile);
+        if (cachedEvent) {
+          const ndkEvent = new NDKEvent(subscription.ndk, cachedEvent);
 
-        subscription.eventReceived(ndkEvent, undefined, true);
-      }
+          subscription.eventReceived(ndkEvent, undefined, true);
+        }
+      });
     });
   },
   async setEvent(event: NDKEvent) {
     // caching only certain types of kinds for now to make sure logic is correct
-    const whitelistedKinds = [Kind.Metadata];
+    const whitelistedKinds = [Kind.Metadata, Kind.RelayList];
 
     if (event.kind === undefined || !whitelistedKinds.includes(event.kind)) {
       return;
     }
 
-    // only cache the most recent event for kind 0
-    if (event.kind === Kind.Metadata) {
+    // only cache the most recent event for kind 0 and kind 10002
+    if ([Kind.Metadata, Kind.RelayList].includes(event.kind)) {
       const key = event.pubkey;
+      const cache =
+        event.kind === Kind.Metadata ? profileEventsCache : relayListCache;
 
       if (
-        !profileEventsCache[key] ||
-        (profileEventsCache[key] &&
+        !cache[key] ||
+        (cache[key] &&
           event.created_at &&
-          event.created_at > profileEventsCache[key].created_at)
+          event.created_at > cache[key].created_at)
       ) {
-        profileEventsCache[key] = await event.toNostrEvent();
+        cache[key] = await event.toNostrEvent();
       }
 
       return;
