9ca4a846e104fa185abb0e6a45056b6486390653
Michael Hall <michaelhall923@protonmail.com>
2023-07-15 16:54:48 -0400

    remove discover feed chips

--- components/DiscoverFeed/DiscoverFeed.tsx
+++ components/DiscoverFeed/DiscoverFeed.tsx
@@ -1,33 +1,10 @@
 import { useMemo, useState } from "react";
-import DiscoverFeedChips from "components/DiscoverFeedChips/DiscoverFeedChips";
-import { Feed, MaxWidthContainer } from "../Feed";
-import { type NDKEvent, type NDKFilter } from "@nostr-dev-kit/ndk";
+import { Feed } from "../Feed";
+import { type NDKFilter } from "@nostr-dev-kit/ndk";
 import { Kind } from "nostr-tools";
 
 export default function DiscoverFeed() {
-  const chipsHeight = 68;
-  const [selectedChip, setSelectedChip] = useState("");
-  const [events, setEvents] = useState<NDKEvent[]>([]);
-  const filter = useMemo<NDKFilter>(() => {
-    const filter: NDKFilter = { kinds: [1, 1808 as Kind], limit: 50 };
-    if (selectedChip) filter["#t"] = [selectedChip];
-    return filter;
-  }, [selectedChip]);
+  const filter: NDKFilter = { kinds: [1, 1808 as Kind], limit: 50 };
 
-  return (
-    <>
-      <MaxWidthContainer>
-        <DiscoverFeedChips
-          events={events}
-          value={selectedChip}
-          onChange={(newValue) => setSelectedChip(newValue as string)}
-        />
-      </MaxWidthContainer>
-      <Feed
-        filter={filter}
-        heightOffset={chipsHeight}
-        onEventsLoaded={setEvents}
-      />
-    </>
-  );
+  return <Feed filter={filter} />;
 }
