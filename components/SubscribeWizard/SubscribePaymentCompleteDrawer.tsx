a8608158416e2b4c2a8eda39befed9181eb2c4d8
Michael Hall <michaelhall923@protonmail.com>
2023-07-27 14:19:00 -0400

    add qr code option

--- components/SubscribeWizard/SubscribePaymentCompleteDrawer.tsx
+++ components/SubscribeWizard/SubscribePaymentCompleteDrawer.tsx
@@ -1,6 +1,7 @@
 import { DrawerProps } from "components/Drawer/Drawer";
 import SubscribeDrawer from "./SubscribeDrawer";
 import {
+  Box,
   Button,
   Center,
   CopyButton,
@@ -23,6 +24,7 @@ import useAuth from "hooks/useAuth";
 import { fetchSubscriptionStatus, setSubscriptionStatus } from "store/Auth";
 import { useDispatch } from "react-redux";
 import { isDesktop } from "react-device-detect";
+import QRCode from "qrcode.react";
 
 type SubscribePaymentCompleteDrawerProps = DrawerProps & {
   onClose: () => void;
@@ -104,42 +106,51 @@ export default function SubscribePaymentCompleteDrawer({
       <Text c="white" ta="center" fz={20} fw="bold" mt={8}>
         {isPaymentComplete ? "You're on your way!" : "Almost there!"}
       </Text>
-      <Group spacing={4} mt={32} c="white">
-        <ProfileLink size={42} />
-        <ArrowRightIcon width={16} height={16} />
-        <Image src="/logo.svg" width={42} />
+      <Group mt={32} position="apart">
+        <Box>
+          <Group spacing={4} c="white">
+            <ProfileLink size={42} />
+            <ArrowRightIcon width={16} height={16} />
+            <Image src="/logo.svg" width={42} />
+          </Group>
+          <Text c="white" fz={20} fw="bold" mt={24} lh={1}>
+            {selectedPassOption?.priceSATS.toLocaleString()} sats
+          </Text>
+          <Text color="green.6" fz={50} fw="bold">
+            {selectedPassOption?.numDays} days
+          </Text>
+          <Group spacing={10} mt="xs">
+            <Center
+              w={38}
+              h={38}
+              c={isPaymentComplete ? "green.5" : "gray.5"}
+              sx={(theme) => ({
+                border: "2px solid",
+                borderRadius: "50%",
+                backgroundColor: isPaymentComplete
+                  ? theme.fn.rgba(theme.colors.green[5], 0.15)
+                  : theme.fn.rgba(theme.colors.gray[5], 0.15),
+              })}
+            >
+              {isPaymentComplete ? <CheckIcon /> : <EllipsisIcon />}
+            </Center>
+            <Text fz="sm" fw="bold">
+              {isPaymentComplete ? "Payment successful" : "Awaiting payment..."}
+            </Text>
+          </Group>
+          {isPaymentComplete && (
+            <Text mt={40} fz="sm">
+              You're good to go! Once your time is up, you can pay again to
+              regain access to sharing and commenting on Stemstr.
+            </Text>
+          )}
+        </Box>
+        {isDesktop && invoice && !isPaymentComplete && (
+          <Center h={160} w={160} bg="white" sx={{ borderRadius: 10 }}>
+            <QRCode size={144} value={invoice} />
+          </Center>
+        )}
       </Group>
-      <Text c="white" fz={20} fw="bold" mt={24} lh={1}>
-        {selectedPassOption?.priceSATS.toLocaleString()} sats
-      </Text>
-      <Text color="green.6" fz={50} fw="bold">
-        {selectedPassOption?.numDays} days
-      </Text>
-      <Group spacing={10} mt="xs">
-        <Center
-          w={38}
-          h={38}
-          c={isPaymentComplete ? "green.5" : "gray.5"}
-          sx={(theme) => ({
-            border: "2px solid",
-            borderRadius: "50%",
-            backgroundColor: isPaymentComplete
-              ? theme.fn.rgba(theme.colors.green[5], 0.15)
-              : theme.fn.rgba(theme.colors.gray[5], 0.15),
-          })}
-        >
-          {isPaymentComplete ? <CheckIcon /> : <EllipsisIcon />}
-        </Center>
-        <Text fz="sm" fw="bold">
-          {isPaymentComplete ? "Payment successful" : "Awaiting payment..."}
-        </Text>
-      </Group>
-      {isPaymentComplete && (
-        <Text mt={40} fz="sm">
-          You're good to go! Once your time is up, you can pay again to regain
-          access to sharing and commenting on Stemstr.
-        </Text>
-      )}
       {isPaymentComplete ? (
         <Button onClick={onClose} mt={90} fullWidth>
           Let's go!
