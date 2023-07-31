9e2e0370bdb45eb3c1fa9f6eb3981c91237096e3
Michael Hall <michaelhall923@protonmail.com>
2023-07-31 16:01:59 -0400

    add console.logs

--- hooks/useReferrer.tsx
+++ hooks/useReferrer.tsx
@@ -13,16 +13,19 @@ const useReferrer = (): ReferrerInfo => {
     // Get the referrer URL when the component mounts
     referrer.current = document.referrer;
 
+    console.log("1: " + document.referrer);
     // Check if the referrer is from the same origin (within the app)
     if (referrer.current) {
       try {
         const referrerUrl = new URL(referrer.current);
+        console.log("2: " + referrerUrl.origin, window.location.origin);
         setIsFromSameOrigin(referrerUrl.origin === window.location.origin);
       } catch (e) {
         // If the referrer URL is not a valid URL, it's not from the same origin
         setIsFromSameOrigin(false);
       }
     } else {
+      console.log("3:");
       setIsFromSameOrigin(false);
     }
   }, []);
