4a06f290536072c3ebdc39186b79269da8e98fb8
Michael Hall <michaelhall923@protonmail.com>
2023-07-31 17:55:34 -0400

    remove nip05 from repost headers

--- components/NoteHeader/NoteRepostHeader.tsx
+++ components/NoteHeader/NoteRepostHeader.tsx
@@ -1,16 +1,10 @@
 import { Group, Text } from "@mantine/core";
-import { RepostIcon, VerifiedIcon } from "icons/StemstrIcon";
+import { RepostIcon } from "icons/StemstrIcon";
 import { useEvent } from "ndk/NDKEventProvider";
-import useNip05 from "ndk/hooks/useNip05";
 import { getFormattedName } from "ndk/utils";
-import { Nip05Status } from "store/Nip05";
 
 export default function NoteRepostHeader() {
   const { repostedBy } = useEvent();
-  const repostedByNip05Status = useNip05(
-    repostedBy?.hexpubkey(),
-    repostedBy?.profile?.nip05
-  );
 
   return (
     <Group spacing={0} c="white" fz="sm" lh="normal">
@@ -18,12 +12,6 @@ export default function NoteRepostHeader() {
       <Text ml={8} fw="bold" span>
         {getFormattedName(repostedBy)}
       </Text>
-      {repostedByNip05Status === Nip05Status.Valid && (
-        <>
-          &nbsp;
-          <VerifiedIcon width={14} height={14} />
-        </>
-      )}
       <Text c="gray.3" span>
         &nbsp;reposted
       </Text>
