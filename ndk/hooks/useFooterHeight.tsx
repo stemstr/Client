1c6ca24f9093f96fb15d4fe643794cac51d9bfd5
Michael Hall <michaelhall923@protonmail.com>
2023-08-02 12:08:12 -0400

    get rid of bottomnav padding on non-notched devices

--- ndk/hooks/useFooterHeight.tsx
+++ ndk/hooks/useFooterHeight.tsx
@@ -1,9 +1,11 @@
 import { useMediaQuery } from "@mantine/hooks";
-import { isPwa } from "../../utils/common";
+import { hasNotch, isPwa } from "../../utils/common";
 import { PWA_BOTTOM_BUFFER } from "../../constants/styles";
 
 export default function useFooterHeight() {
   const footerHeight = useMediaQuery(`(max-width: 480px)`) ? 64 : 96;
 
-  return isPwa() ? footerHeight + PWA_BOTTOM_BUFFER : footerHeight;
+  return isPwa() && hasNotch()
+    ? footerHeight + PWA_BOTTOM_BUFFER
+    : footerHeight;
 }
