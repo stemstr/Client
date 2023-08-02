1e48c9c2b092d17575b726a00b8ccbd7a6f34dda
Michael Hall <michaelhall923@protonmail.com>
2023-08-02 15:21:52 -0400

    remove duplicate thumb colors

--- ndk/hooks/useProfilePicSrc.tsx
+++ ndk/hooks/useProfilePicSrc.tsx
@@ -12,27 +12,16 @@ export default function useProfilePicSrc(user?: NDKUser) {
       scale: 75,
       shapeColor: [
         "EAE2FC", // purple.0
-        "EAE2FC", // purple.1
         "BFAAEA", // purple.2
-        "BFAAEA", // purple.3
-        "BFAAEA", // purple.4
         "865AE2", // purple.5
-        "865AE2", // purple.6
         "763AF4", // purple.7
         "41355C", // purple.8
-        "41355C", // purple.9
       ],
       backgroundColor: [
         "8BF6E3", // green.0
-        "8BF6E3", // green.1
         "09D4B0", // green.2
-        "09D4B0", // green.3
-        "09D4B0", // green.4
         "07B898", // green.5
-        "07B898", // green.6
-        "07B898", // green.7
         "0B6252", // green.8
-        "0B6252", // green.9
       ],
       backgroundType: ["gradientLinear"],
     });
