07049f78a9cbe07baf6414e69f8e9c97af5d0e60
Michael Hall <michaelhall923@protonmail.com>
2023-08-01 20:56:26 -0400

    fix nip05 cors maybe

--- next.config.js
+++ next.config.js
@@ -10,6 +10,27 @@ const withPWA = require("next-pwa")({
 
 module.exports = withPWA(
   withBundleAnalyzer({
+    async headers() {
+      return [
+        {
+          // CORS for nip05
+          source: "/.well-known/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
+    },
     reactStrictMode: false,
     eslint: {
       ignoreDuringBuilds: true,
