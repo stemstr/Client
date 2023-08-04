7fb3206388034f78050b8fb22cc94cd8f5892c38
Michael Hall <michaelhall923@protonmail.com>
2023-08-04 10:16:56 -0400

    add bottom padding to settings page

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -10,10 +10,12 @@ import Head from "next/head";
 import { SettingNsec } from "components/Settings/SettingNsec";
 import { SettingDefaultZapWallet } from "../../components/Settings/SettingDefaultZapWallet";
 import { SettingDeveloperMode } from "components/Settings/SettingDeveloperMode";
+import useFooterHeight from "ndk/hooks/useFooterHeight";
 
 export default function Settings() {
   const { guardAuth, isAuthenticated } = useAuth();
   guardAuth();
+  const footerHeight = useFooterHeight();
 
   return isAuthenticated ? (
     <>
@@ -28,7 +30,14 @@ export default function Settings() {
           Account settings
         </Text>
       </Group>
-      <Stack pt="md" pl="md" pr="md" spacing="sm" align="center">
+      <Stack
+        pt="md"
+        pl="md"
+        pr="md"
+        pb={footerHeight + 16}
+        spacing="sm"
+        align="center"
+      >
         <SettingPubkey />
         <SettingNsec />
         {/* wallet chooser is only used on mobile */}
