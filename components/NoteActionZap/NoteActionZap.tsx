94f2d86a7ed94180df4b199318fa0a2719c6b03a
bndw <ben@bdw.to>
2023-08-29 11:23:52 -0700

    fix exception raised on invalid lud06

--- components/NoteActionZap/NoteActionZap.tsx
+++ components/NoteActionZap/NoteActionZap.tsx
@@ -71,10 +71,16 @@ const NoteActionContentWithZapWizard = () => {
 const NoteActionZap = () => {
   const { event } = useEvent();
   const zapRecipient = useUser(event.pubkey);
-  const isZappable =
-    zapRecipient && getLnurlServiceEndpoint(zapRecipient.profile, event);
+  let isZappable = false as boolean | undefined;
 
-  return isZappable ? (
+  try {
+    isZappable =
+      zapRecipient && !!getLnurlServiceEndpoint(zapRecipient.profile, event);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return isZappable && zapRecipient ? (
     <ZapWizardProvider zapRecipient={zapRecipient} zappedEvent={event}>
       <NoteActionContentWithZapWizard />
     </ZapWizardProvider>
